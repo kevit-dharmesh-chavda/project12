18e7a9bba2daa3a274f087177f1469c984e1d14c
dharmeshKevit <dharmesh_chavda@kevit.io>
2022-06-14 17:46:29 +0530

    index.js is commited

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.delete('/del_user', function (req, res) {
 })
 
 
+
 // This responds a GET request for the /list_user page.
 app.get('/list_user', function (req, res) {
    console.log("Got a GET request for /list_user");
